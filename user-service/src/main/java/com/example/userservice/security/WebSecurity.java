28616095acd9e4aa9e8be1ad5b9df5689dcb49c0
JeonSangYong <tkddyd420@naver.com>
2023-06-06 16:14:46 +0900

    [강의] Users Microservice - Spring Security 연동

---
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -0,0 +1,26 @@
+package com.example.userservice.security;
+
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+
+    private static final String[] WHITE_LIST = {"/users/**", "/**"};
+
+    @Bean
+    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .headers(authorize -> authorize.frameOptions().disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .antMatchers(WHITE_LIST).permitAll()
+                        .requestMatchers(PathRequest.toH2Console()).permitAll())
+                .build();
+    }
+}
