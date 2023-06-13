bb2830029d7e485431f1583210ec4017dc5b1d55
JeonSangYong <tkddyd420@naver.com>
2023-06-13 19:24:15 +0900

    [강의] Users Microservice - AuthenticationFilter, loadUserByUsername() 구현

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,25 +1,55 @@
 package com.example.userservice.security;
 
 
+import com.example.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
 
+@Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurity {
 
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ObjectPostProcessor<Object> objectPostProcessor;
     private final String[] WHITE_LIST = {"/users/**", "/**"};
 
     @Bean
     protected SecurityFilterChain config(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
-                .headers(authorize -> authorize.frameOptions().disable())
-                .authorizeHttpRequests(authorize -> authorize
-                        .antMatchers(WHITE_LIST).permitAll()
-                        .requestMatchers(PathRequest.toH2Console()).permitAll())
-                .build();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.authorizeRequests()
+                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                .antMatchers("/error/**").permitAll()
+                .antMatchers(WHITE_LIST).hasIpAddress("192.168.25.43")
+                .and()
+                .addFilter(getAuthenticationFilter());
+
+        return http.build();
     }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        AuthenticationManagerBuilder builder = new AuthenticationManagerBuilder(objectPostProcessor);
+        authenticationFilter.setAuthenticationManager(authenticationManager(builder));
+        return authenticationFilter;
+    }
+
+    private AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+        return auth.build();
+    }
+
+
 }
