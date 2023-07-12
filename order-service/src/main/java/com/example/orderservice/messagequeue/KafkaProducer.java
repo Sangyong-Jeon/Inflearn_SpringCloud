3f487c6ce1d8ef6ea986720e3fd939ff33370fbb
JeonSangYong <tkddyd420@naver.com>
2023-07-12 22:26:54 +0900

    [강의] Kafka를 활용한 데이터 동기화 수정

--- order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducer.java
+++ order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducer.java
@@ -21,43 +21,18 @@ import java.util.List;
 public class KafkaProducer {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
-    private final List<Field> fields = Arrays.asList(
-            new Field("string", true, "order_id"),
-            new Field("string", true, "user_id"),
-            new Field("int32", true, "product_id"),
-            new Field("int32", true, "qty"),
-            new Field("int32", true, "unit_price"),
-            new Field("int32", true, "total_price")
-    );
-    private final Schema schema = Schema.builder()
-            .type("struct")
-            .fields(fields)
-            .optional(false)
-            .name("orders")
-            .build();
 
     public OrderDto send(String topic, OrderDto orderDto) {
-        Payload payload = Payload.builder()
-                .order_id(orderDto.getOrderId())
-                .user_id(orderDto.getUserId())
-                .product_id(orderDto.getProductId())
-                .qty(orderDto.getQty())
-                .unit_price(orderDto.getUnitPrice())
-                .total_price(orderDto.getTotalPrice())
-                .build();
-
-        KafkaOrderDto kafkaOrderDto = new KafkaOrderDto(schema, payload);
-
         ObjectMapper mapper = new ObjectMapper();
         String jsonInString = "";
         try {
-            jsonInString = mapper.writeValueAsString(kafkaOrderDto);
+            jsonInString = mapper.writeValueAsString(orderDto);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
 
         kafkaTemplate.send(topic, jsonInString);
-        log.info("Order Producer sent data from the Order microservice: " + kafkaOrderDto);
+        log.info("Kafka Producer sent data from the Order microservice: " + orderDto);
 
         return orderDto;
     }
